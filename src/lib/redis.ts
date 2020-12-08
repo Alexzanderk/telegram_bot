e4765872b282da6a33257a158cc6a1288f4f785a
Alexzanderk <kotsarev.a@gmail.com>
2020-12-08 15:48:51 +0200

    add redis, change docker compose

---
+++ src/lib/redis.ts
@@ -0,0 +1,35 @@
+import { redis } from '../services';
+import RedisIO from 'ioredis';
+
+interface IRedis {
+  getFromRedis(key: string): Promise<any>;
+  setExpireToRedis(key: string, data: any, ttl?: number): Promise<string>;
+  
+}
+
+export class Redis implements IRedis {
+  private redis: RedisIO.Redis;
+
+  constructor(newInstanse: boolean = false) {
+    this.redis = redis(newInstanse);
+  }
+
+  public async getFromRedis(key: string): Promise<any> {
+    try {
+      const result = await this.redis.get(key);
+      const parsedResult = JSON.parse(result);
+      return parsedResult;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async setExpireToRedis(key: string, data: any, ttl?: number): Promise<string> {
+    try {
+      const dataStringify = JSON.stringify(data);
+      return await this.redis.set(key, dataStringify, 'EX', ttl);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
---
+++ src/services/Redis.ts
@@ -0,0 +1,22 @@
+import RedisIO = require('ioredis');
+
+const redisOpts: RedisIO.RedisOptions = {
+  retryStrategy: (times: number) => Math.min(times * 10000, 15000),
+  reconnectOnError: (err: Error) => err.message.includes('READONLY'),
+  connectTimeout: 15000,
+};
+
+const getRedis = () => new RedisIO(<string>process.env.REDIS_CONNECTION_STRING, redisOpts);
+
+let redisInstance: RedisIO.Redis;
+
+export function redis(newInstance: boolean = false): RedisIO.Redis {
+  if (newInstance) {
+    return getRedis();
+  }
+
+  if (!redisInstance) {
+    redisInstance = getRedis();
+  }
+  return redisInstance;
+}
